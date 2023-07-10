b93c2e75e648ee1addb6f431128744703358558c
geetamatkar <geetamatkar1@gmail.com>
2023-07-09 19:15:36 -0500

    # Updated Project section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -101,12 +101,12 @@ const Projects = () => {
                     variants={container}
                  >
                     {/* Row 1 */}
-                    <div
+                    {/*<div
                       className='flex justify-center text-center items-center p-10 bg-red
                       max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold'
                     >
                         USER INTERFACES
-                    </div>
+                    </div>*/}
 
                   
                   
